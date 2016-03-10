7d37487676680cbcfc7826b2433d0fa60cfc79b3
Cameron Roark <cam@cameronroark.com>
2016-03-10 08:48:22 -0800

    RobotPaths Update

--- RobotPaths.js
+++ RobotPaths.js
@@ -1,3 +1,13 @@
+/*
+A robot located at the top left corner of an n x n
+grid is trying to reach the bottom right corner.
+The robot can move either up, down, left, or right,
+ but cannot visit the same spot twice. How many possible
+ unique paths are there to the bottom right corner?
+
+Make your solution work for a grid of any size.
+*/
+
 function robotPaths (n) {
   var solutions = 0;
   var board = makeBoard(n);
