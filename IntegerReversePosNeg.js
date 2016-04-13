f6539aadb7c94a3fb3976dfd1e046b6263a0e68f
Cameron Roark <cam@cameronroark.com>
2016-04-13 10:13:28 -0700

    Improved algorithms for intReverse

--- IntegerReversePosNeg.js
+++ IntegerReversePosNeg.js
@@ -4,12 +4,7 @@ Only use integers and math in your solution.
 */
 function reverseInteger(number) {
     var ret = 0;
-    var count = 0;
     while (number !== 0) {
-      count++;
-      if(count > 10) {
-        return;
-      }
         ret = ret * 10 + number % 10;
         number = number > 0 ? Math.floor(number / 10) : Math.ceil(number/10);
     }
--- PositiveIntegerReverse.js
+++ PositiveIntegerReverse.js
@@ -6,13 +6,9 @@ Only use integers and math in your solution.
 
 function reverseInteger(number) {
     var ret = 0;
-    console.log('Starting number count: '+number);
     while (number !== 0) {
         ret = ret * 10 + number % 10;
-        console.log(ret);
         number = Math.floor(number / 10);
-        console.log(number);
     }
-    console.log('Return Value: '+ret);
     return Math.floor(ret);
 };
